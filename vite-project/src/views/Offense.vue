547a45171f653413c0f2952f8e5cad86a1da22a8
nathannathan08 <nathanc245@nycstudents.net>
2025-03-10 14:01:23 -0400

    f

--- vite-project/src/views/Offense.vue
+++ vite-project/src/views/Offense.vue
@@ -1,10 +1,121 @@
+
 <template>
-    <div class="offense">
-      <h1>offense</h1>
-    </div>
-  </template>
+  <div>
+    <h1>Hate Crime Data in NYC - Motive</h1>
+
+    <canvas id="offenseChart"></canvas>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, computed, nextTick } from "vue";
+import Chart from "chart.js/auto"; 
+
+const hate = ref([]);
+const offenseChart = ref(null);
+
+
+async function getData() {
+  let res = await fetch("https://data.cityofnewyork.us/resource/bqiq-cu78.json");
+  let data = await res.json();
+  hate.value = data;
+}
+
+
+const OffenseWithCounts = computed(() => {
+  const OffenseCounts = hate.value.reduce((acc, item) => {
+    const category = item.offense_description || "Unknown";
+    acc[category] = (acc[category] || 0) + 1;
+    return acc;
+  }, {});
+
+
+  const categories = Object.keys(OffenseCounts);
+  const categorizedData = categories.reduce((acc, category) => {
+    const count = OffenseCounts[category];
+    if (count < 10) {
+      acc.Other = (acc.Other || 0) + count;
+    } else {
+      acc[category] = count;
+    }
+    return acc;
+  }, {});
+
+  return Object.keys(categorizedData).map(category => ({
+    offense_description: category,
+    count: categorizedData[category]
+  }));
+});
+
+
+const renderChart = () => {
+  const chartData = {
+    labels: OffenseWithCounts.value.map(item => item.offense_description),
+    datasets: [
+      {
+        label: "Offense Counts",
+        data: OffenseWithCounts.value.map(item => item.count),
+        backgroundColor: ["#8a0017"], 
+        hoverOffset: 4
+      }
+    ]
+  };
+
+
+  offenseChart.value = new Chart(document.getElementById("offenseChart"), {
+    type: "bar",
+    data: chartData,
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: "top",
+        },
+        tooltip: {
+          callbacks: {
+            label: function (tooltipItem) {
+              return `${tooltipItem.label}: ${tooltipItem.raw} incidents`;
+            },
+          },
+        },
+      },
+    },
+  });
+};
+
+
+
+onMounted(async () => {
+  await getData();
+  nextTick(() => renderChart()); 
+});
+
+</script>
+
+<style scoped>
+canvas {
+  max-width: 900px; 
+  width: 100%;     
+  height: 300px;  
+  margin: 0 auto;  
   
-  <style>
-  
-  </style>
-  
+}
+ul {
+  list-style-type: none;
+    
+}
+
+li {
+  border: 1px solid #ddd;
+  margin: 10px 0;
+  padding: 10px;
+   
+}
+
+p {
+  margin: 5px 0;
+  color: rgb(168, 0, 22);
+  font-size: 35px;
+    
+}
+</style>
